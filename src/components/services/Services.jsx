0534add948eb70838def7e82f6fe233c2972593e
adhishmalhotra <adhish.malhotra@ontariotechu.net>
2022-10-05 10:53:15 -0400

    adding files to the repos

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './services.css'
+import IMG1 from '../../assets/services1.jpg'
+import IMG2 from '../../assets/services2.jpg'
 
 const Services = () => {
   return (
@@ -10,7 +12,37 @@ const Services = () => {
       <div className='container services__container'>
         <article className='service__item'>
         <div className='service__item-image'>
-          <img src="" alt="" />
+          <img src={IMG2} alt="Omers" />
+        </div>
+        <h3>Backend Developer</h3>
+        <div className='service__item-cta'>
+          <p>
+
+          </p>
+        </div>
+        </article>
+
+        <article className='service__item'>
+        <div className='service__item-image'>
+          <img src={IMG1} alt="Ontario Tech University" />
+        </div>
+        <h3>Technical Services Assistant</h3>
+        <div className='service__item-cta'>
+          <p>
+            
+          </p>
+        </div>
+        </article>
+
+        <article className='service__item'>
+        <div className='service__item-image'>
+          <img src={IMG1} alt="Ontario Tech University" />
+        </div>
+        <h3>Lab Assistant</h3>
+        <div className='service__item-cta'>
+          <p>
+            
+          </p>
         </div>
         </article>
       </div>
